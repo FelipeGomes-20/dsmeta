5c67f6b5bcffb71859fb8e6e1e05af20ee9cea67
Felipe <felipe.gomes@sptech.school>
2022-07-15 18:11:55 -0300

    Axios, useEffect first request

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -9,7 +9,7 @@ function Header() {
                 <h1>DSMeta</h1>
                 <p>
                     Desenvolvido por
-                    <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+                    <a href="https://www.instagram.com/choice_81/"> @choice_81 </a>/<a href="https://github.com/FelipeGomes-20"> FelipeGomes-20</a>
                 </p>
             </div>
         </header>
